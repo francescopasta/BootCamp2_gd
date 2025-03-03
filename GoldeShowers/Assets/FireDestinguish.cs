0c679285928b1cedbce950e3cdd6eb628b63c64c
Lyubomir Kolev <lubokolev14@gmail.com>
2025-03-03 16:44:30 +0100

    FireLogic

--- GoldeShowers/Assets/FireDestinguish.cs
+++ GoldeShowers/Assets/FireDestinguish.cs
@@ -2,15 +2,23 @@ using UnityEngine;
 
 public class FireDestinguish : MonoBehaviour
 {
+    public float timeToDie;
+    public Fire fireScript;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         
     }
-
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (timeToDie <= 0)
+        {
+            gameObject.SetActive(false);
+            fireScript.AssignFires();
+        }
+    }
+    private void OnParticleCollision(GameObject other)
+    {
+        timeToDie -= Time.deltaTime;
     }
 }
