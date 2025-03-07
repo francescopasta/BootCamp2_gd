a79b3ff1b0f5e80a6b165fd633ea0cb86436a4ab
Lyubomir Kolev <lubokolev14@gmail.com>
2025-03-07 01:46:15 +0100

    FireFinal

--- GoldeShowers/Assets/Fire.cs
+++ GoldeShowers/Assets/Fire.cs
@@ -1,49 +1,55 @@
-using NUnit.Framework;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Fire : MonoBehaviour
 {
     public List<Fire> fireList;
-    public float timerToLight;
     public float minTimer;
     public float maxTimer;
-    private float thisTimerToLightUp;
+
+    private float timerToLight;
     public GameObject fire;
+
     private void Start()
     {
-        AssignFires();  
+        AssignFires();
     }
-    // Update is called once per frame
+
     void Update()
     {
-        if (timerToLight <= 0)
+        if (!fire.activeSelf) // Only count down when fire is disabled
         {
-            fire.SetActive(true);
+            timerToLight -= Time.deltaTime;
+            if (timerToLight <= 0)
+            {
+                fire.SetActive(true); // Re-enable fire when timer reaches 0
+            }
         }
-        else
-        {
-            timerToLight -= Time.deltaTime; 
-        }
-
     }
-    public void AssignFires() 
+
+    public void AssignFires()
     {
-        for (int i = 0; i < fireList.Count; ++i)
+        for (int i = 0; i < fireList.Count; i++)
         {
-            fireList[i].timerToLight = Random.Range(minTimer, maxTimer);
+            if (fireList[i].timerToLight == 0)
+            {
+                fireList[i].timerToLight = Random.Range(minTimer, maxTimer);
+            }
+
             if (i > 0)
             {
-                while (fireList[i].timerToLight >= fireList[i - 1].timerToLight + 1 &&
-                       fireList[i].timerToLight <= fireList[i - 1].timerToLight - 1)
+                while (!(fireList[i].timerToLight >= fireList[i - 1].timerToLight + 1 ||
+                         fireList[i].timerToLight <= fireList[i - 1].timerToLight - 1))
                 {
                     fireList[i].timerToLight = Random.Range(minTimer, maxTimer);
                 }
             }
-            if (fireList[i] == this)
-            {
-                thisTimerToLightUp = fireList[i].timerToLight;
-            }
         }
+    }
+
+    public void DisableFire()
+    {
+        fire.SetActive(false);
+        timerToLight = Random.Range(minTimer, maxTimer); // Assign a new countdown timer
     }
 }
--- GoldeShowers/Assets/FireDestinguish.cs
+++ GoldeShowers/Assets/FireDestinguish.cs
@@ -1,9 +1,14 @@
+using UnityEditor.SearchService;
 using UnityEngine;
 
 public class FireDestinguish : MonoBehaviour
 {
     public float timeToDie;
     public Fire fireScript;
+    public float ogTimeToDie;
+
+    public float loseTimer;
+    public float ogloseTime;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -14,11 +19,24 @@ public class FireDestinguish : MonoBehaviour
         if (timeToDie <= 0)
         {
             gameObject.SetActive(false);
-            fireScript.AssignFires();
+        }
+        if (loseTimer > 0)
+        {
+            loseTimer -= Time.deltaTime;    
+        }
+        if (loseTimer <= 0)
+        {
+            Debug.Log("You lose");
         }
     }
     private void OnParticleCollision(GameObject other)
     {
         timeToDie -= Time.deltaTime;
+    }
+    private void OnDisable()
+    {
+        timeToDie = ogTimeToDie;
+        loseTimer = ogloseTime;
+        fireScript.DisableFire();
     }
 }
